f04ac5f5e00be9e5af2edff4f77cd1a8e1708fbf
stk <stanislavc848@gmail.com>
2016-08-17 14:12:08 +0300

    move the pages folder above

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
-import HomePage from './components/pages/HomePage';
-import AboutPage from './components/pages/AboutPage';
-import pageA from './components/pages/pageA';
-import pageB from './components/pages/pageB';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import pageA from './pages/pageA';
+import pageB from './pages/pageB';
 
 export default function Routes(store) {
 
