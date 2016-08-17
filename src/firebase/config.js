968b4c84f805e33b0cc5aa3b37a827421bda0b08
stk <stanislavc848@gmail.com>
2016-08-17 14:16:51 +0300

    new firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAaO4ugEQFToR_ugu6DQoPs8IB06NaXJvo",
+  authDomain: "react-redux-dev.firebaseapp.com",
+  databaseURL: "https://react-redux-dev.firebaseio.com",
+  storageBucket: "react-redux-dev.appspot.com"
+};
